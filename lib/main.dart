4e38a99986e5891dacbb21c30803671df93c30b7
tipo666 <cesarbaez100@gmail.com>
2020-07-11 20:04:27 -0400

    Subiendo el proyecto terminado

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,279 @@
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(Calculadora());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class Calculadora extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
+String strIngreso = "";
+final textControllerIngreso = TextEditingController();
+final textControllerResultado = TextEditingController();
+
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  //Colores para los diferentes botones
+  Color colorBlanco = const Color(0XFFF9FBFB);
+  Color colorGris = Color(0XFF9FBEC3);
+  Color azulOscuro = const Color(0XFF0E5C68);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      backgroundColor: colorGris,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          _panelOperandos(),
+          _panelResultado(),
+          _panelBotones(),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    );
+  }
+
+  ///Método btn
+  ///
+  ///Controla como se ven los botones y su diseño
+  ///
+  ///Mientras se van digitando los numeros los va concatenando.
+  ///
+  ///
+  ///
+
+  Widget _btn(btntext, Color btnColor, Color textColor) {
+    return Container(
+      padding: EdgeInsets.only(bottom: 10.0),
+      child: FlatButton(
+        child: Text(
+          btntext,
+          style: TextStyle(fontSize: 28.0, color: textColor),
         ),
+        onPressed: () {
+          setState(() {
+            textControllerIngreso.text = textControllerIngreso.text + btntext;
+          });
+        },
+        color: btnColor,
+        padding: EdgeInsets.all(18.0),
+        shape: RoundedRectangleBorder(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  ///
+  ///Método btnMasMenos
+  ///
+  ///Control el diseño de como se ve el boton para cambiar de positivo a negativo y viceversa
+  ///
+  ///Al presionar el boton, multiplica el numero actual por -1 para cambiarlo, como String.
+  ///
+  ///
+  Widget _btnMasMenos() {
+    return Container(
+      padding: EdgeInsets.only(bottom: 10.0),
+      child: FlatButton(
+        child:
+            Text("+/-", style: TextStyle(fontSize: 28.0, color: colorBlanco)),
+        onPressed: () {
+          textControllerIngreso.text =
+              (double.parse(textControllerIngreso.text) * -1).toString();
+        },
+        color: colorGris,
+        padding: EdgeInsets.all(18.0),
+        shape: RoundedRectangleBorder(),
+      ),
+    );
+  }
+
+  ///Método btnC
+  ///
+  ///Controla el diseño del boton "Clear" o "Limpiar"
+  ///
+  ///Al presionar el boton, se inicializa tanto el resultado como los operandos y operadores
+  ///
+  ///
+  Widget _btnC(btnTexto, Color btnColor) {
+    return Container(
+      padding: EdgeInsets.only(bottom: 10.0),
+      child: FlatButton(
+        child: Text(
+          btnTexto,
+          style: TextStyle(
+              fontSize: 28.0, color: colorBlanco, fontFamily: 'RobotoMono'),
+        ),
+        //Limpiamos el panel de Operandos.
+        onPressed: () {
+          setState(() {
+            textControllerIngreso.text = "";
+            textControllerResultado.text = "";
+          });
+        },
+        color: btnColor,
+        padding: EdgeInsets.all(18.0),
+        shape: RoundedRectangleBorder(),
+      ),
+    );
+  }
+
+  ///Metodo btnIgual
+  ///
+  ///
+  ///Metodo que controla el diseño del boton "Igual"
+  ///
+  ///Al presionar este botón, se evalua la expresión actual con el operando que se selccionó
+  ///
+  ///
+  Widget _btnIgual(btnText) {
+    return FlatButton(
+      padding: EdgeInsets.all(18.0),
+      shape: RoundedRectangleBorder(),
+      color: azulOscuro,
+      child: Text(
+        btnText,
+        style: TextStyle(fontSize: 28.0, color: colorBlanco),
+      ),
+      onPressed: () {
+        //Validamos que el campo no este vacio para no trabajar con nulos
+        if (textControllerIngreso.text.isNotEmpty) {
+          //Expresion para Parsear
+          Parser p = Parser();
+          ContextModel cm = ContextModel();
+          Expression exp = p.parse(textControllerIngreso.text);
+          setState(() {
+            textControllerResultado.text =
+                exp.evaluate(EvaluationType.REAL, cm).toString();
+          });
+        }
+      },
+    );
+  }
+
+  ///Método panelResultado
+  ///
+  ///Este método controla como se ve el resultado
+  ///
+  ///Tambien presenta el resultado en un TextField
+  ///
+  ///
+  ///
+  _panelResultado() {
+    return Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: TextField(
+          style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.right,
+          controller: textControllerResultado,
+        ));
+  }
+
+  ///Método PanelOperandos
+  ///
+  ///Controla como se ven los operandos y operadores
+  ///
+  ///Inicialmente se presenta un "0"
+  ///
+  ///En este metodo se muestran los operandos y operadores
+  ///
+  ///
+  ///
+  _panelOperandos() {
+    return Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: TextField(
+          decoration: InputDecoration.collapsed(
+              hintText: "0",
+              hintStyle: TextStyle(
+                fontSize: 15,
+              )),
+          style: TextStyle(
+            fontSize: 15,
+          ),
+          textAlign: TextAlign.right,
+          controller: textControllerIngreso,
+        ));
+  }
+
+  ///Método panelBotones
+  ///
+  ///Método que controla el diseño de la matoria de los botones
+  ///
+  ///
+  ///
+  ///
+  ///
+  ///
+
+  _panelBotones() {
+    return Container(
+      color: colorBlanco,
+      child: Column(
+        children: <Widget>[
+          //Separamos el contener del Scaffold visualmente
+          Divider(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              _btnC(
+                'C',
+                colorGris,
+              ),
+              _btnMasMenos(),
+              _btn('%', colorGris, colorBlanco),
+              _btn('/', colorGris, colorBlanco),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              _btn('7', colorBlanco, azulOscuro),
+              _btn('8', colorBlanco, azulOscuro),
+              _btn('9', colorBlanco, azulOscuro),
+              _btn('*', colorGris, colorBlanco),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              _btn('4', colorBlanco, azulOscuro),
+              _btn('5', colorBlanco, azulOscuro),
+              _btn('6', colorBlanco, azulOscuro),
+              _btn('-', colorGris, colorBlanco),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              _btn('1', colorBlanco, azulOscuro),
+              _btn('2', colorBlanco, azulOscuro),
+              _btn('3', colorBlanco, azulOscuro),
+              _btn('+', colorGris, colorBlanco),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              _btn('0', colorBlanco, azulOscuro),
+              _btn('', colorBlanco, azulOscuro),
+              _btn('.', colorBlanco, azulOscuro),
+              _btnIgual('='),
+            ],
+          ),
+          SizedBox(
+            height: 10.0,
+          )
+        ],
+      ),
     );
   }
 }
